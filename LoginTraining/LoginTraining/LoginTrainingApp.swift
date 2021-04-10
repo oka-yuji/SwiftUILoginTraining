cb8c60a6ef541b9f4cd24016fa02b06e50802f21
岡優志 <yu.j.0513@gmail.com>
2021-04-10 10:22:21 +0900

    新規作成

---
+++ LoginTraining/LoginTraining/LoginTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTrainingApp.swift
+//  LoginTraining
+//
+//  Created by 岡優志 on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct LoginTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
